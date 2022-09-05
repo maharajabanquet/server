b8025a24cefdf0a7d85b155701d84241e2991876
Ankit Kumar <support@maharajaraxaul.com>
2022-09-05 15:59:54 +0530

    new changes

--- thirdparty/whatsappweb.js
+++ thirdparty/whatsappweb.js
@@ -2,7 +2,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const  qrcode  = require('qrcode-terminal')
 const axios = require('axios');
 const client = new Client({
-    // authStrategy: new LocalAuth(),
+    authStrategy: new LocalAuth(),
     puppeteer: {
 		args: ['--no-sandbox', '--disable-setuid-sandbox'],
         headless: true
