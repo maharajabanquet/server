c9fe3e9be282127f38ff74ed39d71892777d8b33
Ankit Kumar <support@maharajaraxaul.com>
2022-09-07 19:48:35 +0530

    changes_send

--- scheduler.js
+++ scheduler.js
@@ -1,6 +1,6 @@
 
 require('dotenv/config')
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth, LegacySessionAuth } = require('whatsapp-web.js');
 const mongoose = require('mongoose');
 const booking = require('./models/Booking');
 const qrterminal = require('qrcode-terminal')
@@ -18,66 +18,65 @@ const client = new Client({
         headless: true
 	}
 });
-
-
-class WhatsappBotScheduler {
-    generateQR() {
-        client.on('qr', (qr) => {
-            qrterminal.generate(qr, {small:true})
-        });
-    }
-    
-    clientReady() {
-        let userData = [];
-       
-        client.on('ready', () => {
-            booking.find({}, function(err, data) {
-                if(!data){
-                    console.log('No Booking, Task Skipped');
-                } else {
-                    data.forEach(element => {
-                        const chatId = `917494972470@c.us`;
-                        // const message = `*Hi Praveen, Please clear balance amount 2 Week before ${element.bookingDate} else booking will be cancelled*`
-                        const message = `*Hi Praveen, Please clear balance amount 2 Week before  else booking will be cancelled*`
-
-                        userData.push({chatId: chatId, message: message})
-                        // client.sendMessage(chatId, message)
-                    })
-                    for(let index=0; index<userData.length ; index++) {
-                        client.sendMessage(userData[index].chatId, userData[index].message)
-
-                    }
-                    console.log("TASK COMPLETED");
-                }
-            })
-          });
-    }
-
-    checkPing() {
-        client.on('message_ack', (msg, ack) => {
-            if(ack == 1) {
-                // The message was read
-                process.exit();
-            }
-        });
-    }
-    authenticate() {
-        client.on('authenticated', session => {
-            console.log('authenticated');
-          });
-    }
-    
-    run() {
-        client.initialize();
-    }
-
+function generateQR() {
+    client.on('qr', (qr) => {
+        qrterminal.generate(qr, {small:true})
+    });
 }
 
-const whatsappbot = new WhatsappBotScheduler();
-whatsappbot.generateQR();
-whatsappbot.run();
-whatsappbot.authenticate();
-whatsappbot.clientReady();
-whatsappbot.checkPing();
+function clientReady() {
+    let userData = [];
+    client.on('ready', () => {
+        console.log("READY");
+        booking.find({}, function(err, data) {
+            if(!data){
+                console.log('No Booking, Task Skipped');
+            } else {
+                data.forEach(element => {
+                    const chatId = `917494972470@c.us`;
+                    // const message = `*Hi Praveen, Please clear balance amount 2 Week before ${element.bookingDate} else booking will be cancelled*`
+                    const message = `*Hi Praveen, Please clear balance amount 2 Week before  else booking will be cancelled*`
+
+                    userData.push({chatId: chatId, message: message})
+                    // client.sendMessage(chatId, message)
+                })
+                for(let index=0; index<userData.length ; index++) {
+                    client.sendMessage(userData[index].chatId, userData[index].message)
+
+                }
+                console.log("TASK COMPLETED");
+            }
+        })
+      });
+}
+
+function checkPing() {
+    client.on('message_ack', (msg, ack) => {
+        if(ack == 1) {
+            // The message was read
+            process.exit();
+        }
+    });
+}
+ 
+function authenticate() {
+    client.on('authenticated', session => {
+        
+        console.log('authenticated', session);
+      });
+}
+
+function run() {
+    console.log("running");
+    client.initialize();
+}
+
+// whatsappbot.generateQR();
+// generateQR();
+
+run();
+clientReady();
+authenticate();
+checkPing();
 
 
