e662f42a2f57ef48bacda1d72f699c07d1bb80af
Maharaja Banquet <support@maharajaraxaul.com>
2023-07-08 14:42:36 +0530

    in cash flow ui added

--- routes/cash_inflow.js
+++ routes/cash_inflow.js
@@ -15,12 +15,18 @@ router.get('/get-cash-inflow', (req, res) => {
     // CashInflow.find({}, function(err, result) {
     //     res.status(200).json({data: result})
     // })
+    const type = req && req.query && req.query.type;
+    let query = {}
     const paginate = req && req.query && req.query.paginate;
+    if(type) {
+         query = {transactionType: type}
+    }
     if(paginate) {
         CashInflow.find({}, function(err, result) {
             res.status(200).json({'data': result});
         })
-    } else {
+    } 
+    else {
         CashInflow.paginate({}, {page: Number(req.query.pageNo), limit: Number(req.query.pageSize)}, function(err, result){
             res.status(200).json({'data': result});
         })
@@ -37,5 +43,39 @@ router.get('/deletecashflow', (req, res) => {
         res.status(200).json({status: 'success'});
     })
 })
+
+router.get('/filter-by-type', (req, res) => {
+    const type = req && req.query.type;
+    const query = {transactionType: type};
+    CashInflow.find(query, function(err, result) {
+        if(err) {
+            res.status(404).json({status: 'Not Found'});
+        }
+        res.status(200).json({data: result})
+    })
+})
+
+router.get('/filter-by-date', (req, res) => {
+    const date = req && req.query.date;
+    const query = {transactionDate: date};
+    CashInflow.find(query, function(err, result) {
+        if(err) {
+            res.status(404).json({status: 'Not Found'});
+        }
+        res.status(200).json({data: result})
+    })
+})
+
+router.get('/filter-by-name', (req, res) => {
+    const partyName = req && req.query.name;
+    const query = {partyName: partyName};
+    CashInflow.find(query, function(err, result) {
+        if(err) {
+            res.status(404).json({status: 'Not Found'});
+        }
+        res.status(200).json({data: result})
+    })
+})
+
 
 module.exports = router;
