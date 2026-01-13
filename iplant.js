dfc4a623600d3104dbf0a79cf61a3339fe1eea5e
Ankit Kumar <support@maharajaraxaul.com>
2026-01-14 00:09:09 +0530

    log added

--- iplant.js
+++ iplant.js
@@ -27,52 +27,51 @@ router.post('/plant-analyse', async (req, res) => {
     DHT Temperature: ${payload.dht_temperature},
     DHT Humidity: ${payload.dht_humidity}
   `
-  console.log(prompt);
+  console.log(JSON.stringify(payload));
   
     let base64Image = await iplantModel.findOne({})
     base64Image = base64Image['base64Image']
    
     aiBase64Image = 'data:image/png;base64,' + base64Image.toString()
-  console.log(payload);
   
-   res.status(200)
+   
   if (!aiBase64Image) {
     return res.status(400).json({ error: "Image (base64) is required." });
   }
   
-  // try {
-  //   const response = await openai.chat.completions.create({
-  //     model: "gpt-4o", // Vision model
-  //     messages: [
-  //       {
-  //         role: "user",
-  //         content: [
-  //           { type: "text", text: prompt },
-  //           {
-  //             type: "image_url",
-  //             image_url: {
-  //               url: aiBase64Image,
-  //             },
-  //           },
-  //         ],
-  //       },
-  //     ],
-  //   });
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o", // Vision model
+      messages: [
+        {
+          role: "user",
+          content: [
+            { type: "text", text: JSON.stringify(payload) },
+            {
+              type: "image_url",
+              image_url: {
+                url: aiBase64Image,
+              },
+            },
+          ],
+        },
+      ],
+    });
 
-  //   const aiResponse = response.choices[0].message.content;
-  //   console.log("***********PLANT ANAYLSIS REPORT***********");
-  //   console.log(aiResponse);
-  //   console.log("***********END OF PLANT ANAYLSIS REPORT***********");
-  //   const htmlContent = aiResponse
-  // .replace(/\n/g, '<br>')          // newline to <br>
-  // .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');  // bold markdown to <strong>
-  //   mail(htmlContent, base64Image)
-  //   res.status(200).json({ message: aiResponse });
-  //   res.status(200).json({ message: '' });
-  // } catch (error) {
-  //   console.error("OpenAI API error:", error);
-  //   res.status(500).json({ error: "Something went wrong with OpenAI." });
-  // }
+    const aiResponse = response.choices[0].message.content;
+    console.log("***********PLANT ANAYLSIS REPORT***********");
+    console.log(aiResponse);
+    console.log("***********END OF PLANT ANAYLSIS REPORT***********");
+    const htmlContent = aiResponse
+  .replace(/\n/g, '<br>')          // newline to <br>
+  .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');  // bold markdown to <strong>
+    mail(htmlContent, base64Image)
+    res.status(200).json({ message: aiResponse });
+    res.status(200).json({ message: '' });
+  } catch (error) {
+    console.error("OpenAI API error:", error);
+    res.status(500).json({ error: "Something went wrong with OpenAI." });
+  }
 });
 
 router.post('/capture-base64', async(req, res) => {
